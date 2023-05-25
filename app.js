2be8e66505ef6cb11dcaaf8fdbd7ae75bc77b3a5
Silvia Espindola <silviaespindola@gmail.com>
2023-05-24 22:23:31 -0300

    cambio en app

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.static(publicPath));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo en el puerto 3000')
-})
+});
 
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'));
